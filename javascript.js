1865cb5bfb24fcdeb52f8a82e0532d1ff6edba16
RiyazBy <110787928+RiyazBy@users.noreply.github.com>
2024-08-28 00:37:11 +0100

    Add mouseover and mouseout eventListeners

--- javascript.js
+++ javascript.js
@@ -2,6 +2,17 @@ gridContainer = document.querySelector(".gridContainer");
 for (let i=1; i<=(16*16); i++) {
     const gridCell = document.createElement("div");
     gridCell.classList.add("gridCell");
+    gridCell.style.cssText = "box-sizing: border-box; border: 1px solid black; width : 80px; height: 80px;";
     gridContainer.appendChild(gridCell);
-    gridCell.style.cssText = "box-sizing: border-box; border: 2px solid black; width : 80px; height: 80px;"
 }
+const gridCells = document.querySelectorAll(".gridCell");
+gridCells.forEach(cell => {
+    cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = "blue";
+    });
+})
+gridCells.forEach(cell => {
+    cell.addEventListener("mouseout", () => {
+        cell.style.backgroundColor = "";
+    });
+})
