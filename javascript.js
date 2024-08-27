171435a6e3cc47eb02c3c1b4f3cb97f2bb361321
RiyazBy <110787928+RiyazBy@users.noreply.github.com>
2024-08-27 23:58:19 +0100

    Modify gridCell css style

--- javascript.js
+++ javascript.js
@@ -3,4 +3,5 @@ for (let i=1; i<=(16*16); i++) {
     const gridCell = document.createElement("div");
     gridCell.classList.add("gridCell");
     gridContainer.appendChild(gridCell);
+    gridCell.style.cssText = "box-sizing: border-box; border: 2px solid black; width : 80px; height: 80px;"
 }
