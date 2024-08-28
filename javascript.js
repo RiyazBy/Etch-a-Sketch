ec110ee77f0dbeec1c99f24906f362a5ee4d8ef1
RiyazBy <110787928+RiyazBy@users.noreply.github.com>
2024-08-28 12:48:28 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,10 +2,13 @@ gridContainer = document.querySelector(".gridContainer");
 for (let i=1; i<=(16*16); i++) {
     const gridCell = document.createElement("div");
     gridCell.classList.add("gridCell");
-    gridCell.style.cssText = "box-sizing: border-box; border: 1px solid black; width : 80px; height: 80px;";
+	let initGridCellSide = 720/16;
+	gridCell.style.cssText = "box-sizing: border-box; border: 1px solid black;";
+	gridCell.style.width = initGridCellSide + "px";
+	gridCell.style.height = initGridCellSide + "px";
     gridContainer.appendChild(gridCell);
 }
-const gridCells = document.querySelectorAll(".gridCell");
+let gridCells = document.querySelectorAll(".gridCell");
 gridCells.forEach(cell => {
     cell.addEventListener("mouseover", () => {
         cell.style.backgroundColor = "blue";
@@ -21,16 +24,15 @@ const button = document.querySelector("button");
 button.addEventListener("click", changeGrid);
 function changeGrid() {
 	let input = parseInt(prompt("Enter a number"));
-	let width = 80 * input;
-	let height = 80 * input;
-	gridContainer.style.width = width + "px";
-	gridContainer.style.height = height + "px";
-	let gridCells = document.querySelectorAll(".gridCell");
 	gridCells.forEach(cell => {cell.remove()})
 	for (let i=1; i<=(input**2); i++) {
 		const gridCell = document.createElement("div");
 		gridCell.classList.add("gridCell");
-		gridCell.style.cssText = "box-sizing: border-box; border: 1px solid black; width : 80px; height: 80px;";
+		let newGridCellSide = 720/input;
+		let gridCells = document.querySelectorAll(".gridCell");
+		gridCell.style.cssText = "box-sizing: border-box; border: 1px solid black;";
+		gridCell.style.width = newGridCellSide + "px";
+		gridCell.style.height = newGridCellSide + "px";
 		gridContainer.appendChild(gridCell);
 	}
 	gridCells = document.querySelectorAll(".gridCell");
